2d933d51e8405bf7b0458a5ac5f6f6351eaed9e5
stuart <stuartd@gmail.com>
2026-04-25 16:24:19 +0100

    Initial Commit

---
+++ Photos WallPaper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Photos WallPaper
+//
+//  Created by Stuart Dunkeld on 25/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Photos WallPaper/Photos_WallPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photos_WallPaperApp.swift
+//  Photos WallPaper
+//
+//  Created by Stuart Dunkeld on 25/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Photos_WallPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
