b5a6b5dd5443f55079a3d188078cb41eea816b53
stuart <stuartd@gmail.com>
2026-04-25 22:08:10 +0100

    added stub menubar code

--- Photo Wallpaper/Photo_WallPaperApp.swift
+++ Photo Wallpaper/Photo_WallPaperApp.swift
@@ -1,17 +1,43 @@
 //
-//  Photo_WallpaperApp.swift
+//  Photo_WallPaperApp.swift
 //  Photo Wallpaper
 //
 //  Created by Stuart Dunkeld on 25/04/2026.
 //
 
 import SwiftUI
+import Photos
+import AppKit
 
 @main
-struct Photo_WallpaperApp: App {
+struct PhotoWallpaperApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("Photo Wallpaper", systemImage: "photo") {
+            Button("Change Wallpaper") {
+                showMessage()
+            }
+
+            Divider()
+
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }
         }
+    }
+}
+
+func showMessage() {
+    let alert = NSAlert()
+    alert.messageText = "Change wallpaper?"
+    alert.informativeText = "This will pick a random photo."
+    alert.alertStyle = .warning
+
+    alert.addButton(withTitle: "Go on then")
+    alert.addButton(withTitle: "Cancel")
+
+    let response = alert.runModal()
+
+    if response == .alertFirstButtonReturn {
+        print("User confirmed")
     }
 }
