81a0460b3708806839d35ab95cacbcb29b2a818f
stuartd <stuartd@gmail.com>
2026-04-30 20:46:59 +0100

    still working on plist issue. added gitignore. this time with files

--- photos-wallpaper/photos-wallpaper/photos_wallpaperApp.swift
+++ photos-wallpaper/photos-wallpaper/photos_wallpaperApp.swift
@@ -13,10 +13,16 @@ struct photos_wallpaperApp: App {
     var body: some Scene {
         MenuBarExtra("Wallpaper", systemImage: "photo") {
             Button("Shuffle Now") {
-                WallpaperManager.shared.setRandomPhotoAsWallpaper()
+                if let asset = PhotoManager.shared.getRandomPhoto() {
+                    let size = NSScreen.main?.frame.size ?? CGSize(width: 1920, height: 1080)
+                    PhotoManager.shared.requestImage(for: asset, targetSize: size) { image in
+                        if let image = image {
+                            PhotoManager.shared.setImageAsWallpaper(image)
+                        }
+                    }
+                }
             }
-        
-            
+                    
             Divider()
             
             Button("Quit") {
