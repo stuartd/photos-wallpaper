8797ca6429f4887373d588d017426583aebcde06
stuartd <stuartd@gmail.com>
2026-04-30 18:49:28 +0100

    components wired up now

---
+++ photos-wallpaper/WallpaperManager.swift
@@ -0,0 +1,8 @@
+//
+//  WallpaperManager.swift
+//  photos-wallpaper
+//
+//  Created by Stuart on 30/04/2026.
+//
+
+import Foundation
---
+++ photos-wallpaper/photos-wallpaper/WallpaperManager.swift
@@ -0,0 +1,18 @@
+//
+//  WallpaperManager.swift
+//  photos-wallpaper
+//
+//  Created by Stuart on 30/04/2026.
+//
+
+import Foundation
+import Photos
+import AppKit
+
+class WallpaperManager {
+    static let shared = WallpaperManager()
+
+    func setRandomPhotoAsWallpaper() {
+        // All the logic for requesting access, fetching a photo, saving, and setting wallpaper
+    }
+}
