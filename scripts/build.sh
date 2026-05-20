83bafd970b94fbd92977df92b12c2f2e45ed4579
stuartd <stuartd@gmail.com>
2026-05-20 01:03:05 +0100

    Show git commit in release build about screen

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
-xcodebuild clean build -scheme photos-wallpaper -project photos-wallpaper.xcodeproj
-xcodebuild test -scheme photos-wallpaper -project photos-wallpaper.xcodeproj -destination 'platform=macOS'
+GIT_COMMIT="$(git rev-parse --short HEAD)"
 
+xcodebuild clean build -scheme photos-wallpaper -project photos-wallpaper.xcodeproj GIT_COMMIT="$GIT_COMMIT"
+xcodebuild test -scheme photos-wallpaper -project photos-wallpaper.xcodeproj -destination 'platform=macOS' GIT_COMMIT="$GIT_COMMIT"
